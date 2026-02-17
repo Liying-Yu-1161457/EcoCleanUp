955b056e803c8b479cf137887a81688313b9234e
Liying-Yu-1161457 <Liying.yu@lincolnuni.ac.nz>
2026-02-18 00:33:48 +0800

    success run

--- loginapp/connect.py
+++ loginapp/connect.py
@@ -29,7 +29,7 @@ On PythonAnywhere:
     "user1234$loginexample".
 """
 dbuser = 'postgres'  # PUT YOUR USERNAME HERE - usually "root"
-dbpass = '1234'  # PUT YOUR PASSWORD HERE
+dbpass = '0429'  # PUT YOUR PASSWORD HERE
 dbhost = 'localhost'
 dbport = 5432
 dbname = 'loginexample'
